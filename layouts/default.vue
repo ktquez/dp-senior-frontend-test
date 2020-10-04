4e487ac8104a4b112d5e85dd8f915843a95a44ec
Alan Ktquez <ktquez@gmail.com>
2020-10-04 19:10:55 -0300

    feat(a11y): Add media query for prefers-reduced-motion and device performance

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,15 @@
 <template>
   <nuxt />
 </template>
+
+<style>
+@media screen and (prefers-reduced-motion: reduce), (update: slow) {
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.001ms !important;
+    animation-iteration-count: 1 !important; /* Hat tip Nick/cssremedy (https://css-tricks.com/revisiting-prefers-reduced-motion-the-reduced-motion-media-query/#comment-1700170) */
+    transition-duration: 0.001ms !important;
+  }
+}
+</style>
